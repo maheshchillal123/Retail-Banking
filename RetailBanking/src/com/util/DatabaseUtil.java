4df2aa419ed88315f9a67cf3f56e93ffb2eedb15
maheshchillal123 <46390499+maheshchillal123@users.noreply.github.com>
2020-07-09 13:30:13 +0530

    Update DatabaseUtil.java

--- RetailBanking/src/com/util/DatabaseUtil.java
+++ RetailBanking/src/com/util/DatabaseUtil.java
@@ -9,8 +9,8 @@ public class DatabaseUtil {
 	
 	public static final String URL="jdbc:mysql://localhost:3306/casestudy?characterEncoding=latin1";
 	public static final String DRIVER_NAME="com.mysql.jdbc.Driver";
-	public static final String USERNAME="root";
-	public static final String PASSWORD="root";
+	public static final String USERNAME="";
+	public static final String PASSWORD="";
 	
 	public static Connection getConnection() {
 		Connection con=null;
